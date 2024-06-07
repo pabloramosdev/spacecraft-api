d1ace48b7a82ab7c267c239dd6179b9728e48fbe
pablo ramos <pabloramosdev@gmail.com>
2024-06-07 21:31:19 +0200

    Initial Commit

---
+++ src/main/java/com/movies/spacecraft/SpacecraftApiApplication.java
@@ -0,0 +1,13 @@
+package com.movies.spacecraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacecraftApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpacecraftApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/movies/spacecraft/SpacecraftApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.spacecraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacecraftApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
