7c483ef04f5396f53916eba43fcbc31113815ec9
pablo ramos <pabloramosdev@gmail.com>
2024-06-08 19:06:45 +0200

    feat: create new spacecraft

--- src/main/java/com/movies/spacecraft/controller/SpacecraftController.java
+++ src/main/java/com/movies/spacecraft/controller/SpacecraftController.java
@@ -1,11 +1,15 @@
 package com.movies.spacecraft.controller;
 
+import com.movies.spacecraft.model.SpacecraftRequest;
 import com.movies.spacecraft.model.SpacecraftResponse;
 import com.movies.spacecraft.service.SpacecraftService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +25,11 @@ public class SpacecraftController {
     @GetMapping(path = "/{spacecraftId}", produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<SpacecraftResponse> getSpacecraftById(@PathVariable Long spacecraftId) {
         return ResponseEntity.ok(spacecraftService.findSpacecraftById(spacecraftId));
+    }
+
+    @PostMapping(produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<SpacecraftResponse> postSpacecraft(@RequestBody SpacecraftRequest spacecraftRequest) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(spacecraftService.createSpacecraft(spacecraftRequest));
     }
 
 }
--- src/main/java/com/movies/spacecraft/model/SpacecraftRequest.java
+++ src/main/java/com/movies/spacecraft/model/SpacecraftRequest.java
@@ -1,13 +1,11 @@
 package com.movies.spacecraft.model;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class SpacecraftResponse {
+public class SpacecraftRequest {
     private final String name;
     private final String movie;
     private final String pilot;
