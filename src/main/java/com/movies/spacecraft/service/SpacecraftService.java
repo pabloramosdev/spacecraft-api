35b9f2ea78645e3bfd3939f4fa7798d3986ba574
pablo ramos <pabloramosdev@gmail.com>
2024-06-09 19:36:27 +0200

    feat: add complete update and partial update of spacecrafts

--- src/main/java/com/movies/spacecraft/service/SpacecraftService.java
+++ src/main/java/com/movies/spacecraft/service/SpacecraftService.java
@@ -1,5 +1,8 @@
 package com.movies.spacecraft.service;
 
+import com.movies.spacecraft.repository.entity.Spacecraft;
+import com.movies.spacecraft.service.exception.SpacecraftNotFoundException;
+import com.movies.spacecraft.service.exception.SpacecraftNotValidException;
 import com.movies.spacecraft.service.model.SpacecraftRequest;
 import com.movies.spacecraft.service.model.SpacecraftResponse;
 import com.movies.spacecraft.repository.SpacecraftRepository;
@@ -16,9 +19,7 @@ public class SpacecraftService {
     private final SpacecraftRepository spacecraftRepository;
 
     public SpacecraftResponse findSpacecraftById(Long spacecraftId) {
-        return spacecraftRepository.findById(spacecraftId)
-                .map(spacecraftMapper::toSpacecraftResponse)
-                .orElseGet(() -> SpacecraftResponse.builder().build());
+        return spacecraftMapper.toSpacecraftResponse(findSpacecraft(spacecraftId));
     }
 
     @Transactional
@@ -26,4 +27,39 @@ public class SpacecraftService {
         return spacecraftMapper.toSpacecraftResponse(
                 spacecraftRepository.save(spacecraftMapper.toSpacecraft(spacecraftRequest)));
     }
+
+    @Transactional
+    public SpacecraftResponse updateSpacecraft(Long spacecraftId, SpacecraftRequest spacecraftRequest) {
+        Spacecraft spacecraft = findSpacecraft(spacecraftId);
+        if (spacecraftRequest != null && spacecraftRequest.getName() != null && spacecraftRequest.getMovie() != null &&
+            spacecraftRequest.getPilot() != null) {
+            spacecraft.setName(spacecraftRequest.getName());
+            spacecraft.setMovie(spacecraftRequest.getMovie());
+            spacecraft.setPilot(spacecraftRequest.getPilot());
+        } else {
+            throw new SpacecraftNotValidException();
+        }
+        return spacecraftMapper.toSpacecraftResponse(spacecraftRepository.save(spacecraft));
+    }
+
+    @Transactional
+    public SpacecraftResponse partialUpdateSpacecraft(Long spacecraftId, SpacecraftRequest spacecraftRequest) {
+        Spacecraft spacecraft = findSpacecraft(spacecraftId);
+        if (spacecraftRequest != null && spacecraftRequest.getName() != null) {
+            spacecraft.setName(spacecraftRequest.getName());
+        }
+        if (spacecraftRequest != null && spacecraftRequest.getMovie() != null) {
+            spacecraft.setMovie(spacecraftRequest.getMovie());
+        }
+        if (spacecraftRequest != null && spacecraftRequest.getPilot() != null) {
+            spacecraft.setPilot(spacecraftRequest.getPilot());
+        }
+        return spacecraftMapper.toSpacecraftResponse(spacecraftRepository.save(spacecraft));
+    }
+
+    private Spacecraft findSpacecraft(Long spacecraftId) {
+        return spacecraftRepository.findById(spacecraftId)
+                .orElseThrow(() -> new SpacecraftNotFoundException(spacecraftId));
+    }
+
 }
